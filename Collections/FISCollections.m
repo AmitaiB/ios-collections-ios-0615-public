c5c9221ffbefd8d4e3f530e3afbcc83258c15ae7
AmitaiB <blickstein@gmail.com>
2015-06-04 15:38:24 -0400

    6-nested loops method --> very confusing

--- Collections/FISCollections.m
+++ Collections/FISCollections.m
@@ -127,21 +127,35 @@
     return outputDictionary;
 }
 
--(NSDictionary *)organizeSongsByArtist:(NSArray*)songsToOrganize
+-(NSDictionary *)organizeSongsByArtist:(NSArray*)playList
 {
-    NSMutableDictionary *dictionaryOfOrganizedSongs = [[NSMutableDictionary alloc] init];
+    NSMutableArray *artistNoHyphenSong = [[NSMutableArray alloc] init];
+    NSMutableArray *artistThenSongTrimmed = [[NSMutableArray alloc] init];
+    //"for" loop is Phase I --> II
+    for (NSString *artistHyphenSong in playList) {
+        [artistNoHyphenSong addObject: [artistHyphenSong componentsSeparatedByString:@"-"]]; //-->Phase III
+        for (NSString *artistThenSong in artistNoHyphenSong) {
+            artistThenSong
+        }
+    }
     
-//    breaks the string into [Artist -] [song]
-    NSMutableArray* songArtistInArrayForm = [[songsToOrganize[0] componentsSeparatedByString:@"-"] mutableCopy];
-    
-    //Just the first song, first
-    songArtistInArrayForm[0] = [songArtistInArrayForm[0] mutableCopy];
-    [songArtistInArrayForm[0] replaceOccurrencesOfString:@"-" withString:@" " options:NSCaseInsensitiveSearch range:NSMakeRange(0, [[songArtistInArrayForm[0] length]])];
-    NSDictionary* songByArtist = [NSDictionary dictionaryWithObjects:songArtistInArrayForm[1] forKeys:songArtistInArrayForm[0]];
+    NSString *startSmall = songsToOrganize[0];
     
     
     
-    return dictionaryOfOrganizedSongs;
+//    NSMutableDictionary *dictionaryOfOrganizedSongs = [[NSMutableDictionary alloc] init];
+//    
+////    breaks the string into [Artist -] [song]
+//    NSMutableArray* songArtistInArrayForm = [[songsToOrganize[0] componentsSeparatedByString:@"-"] mutableCopy];
+//    
+//    //Just the first song, first
+//    songArtistInArrayForm[0] = [songArtistInArrayForm[0] mutableCopy];
+//    [songArtistInArrayForm[0] replaceOccurrencesOfString:@"-" withString:@" " options:NSCaseInsensitiveSearch range:NSMakeRange(0, [[songArtistInArrayForm[0] length]])];
+//    NSDictionary* songByArtist = [NSDictionary dictionaryWithObjects:songArtistInArrayForm[1] forKeys:songArtistInArrayForm[0]];
+//    
+//    
+//    
+//    return dictionaryOfOrganizedSongs;
 }
 
 @end
